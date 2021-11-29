f4aec1368309dbddcebcae6dd54b061848c04d07
Figarillo <axel.leonardi@hotmail.com>
2021-11-29 18:04:55 -0300

    Eliminación de scripts inutiles

--- js/op/substract.js
+++
@@ -1,9 +0,0 @@
-export const doSub = (curVal, prevVal, screen) => {
-	let result;
-	if (prevVal == 0) {
-		screen.viewOperation('-');
-	} else {
-		result = parseFloat(curVal) - parseFloat(prevVal);
-		screen.viewResult(result);
-	}
-};
--- js/op/sum.js
+++
@@ -1,1 +0,0 @@
-export const doSum = (num1, num2) => num1 + num2;
